d5ab3163ea83477c69693486b15db535c85c327b
Matt Bing <matt@mutedwarf.com>
2002-07-03 01:04:20 +0000

    more indentation/comment nits

--- cache.h
+++ cache.h
@@ -1,8 +1,8 @@
 /*
-  Please see tcpprep.c for license information.
-
-  Copyright (c) 2001 Aaron Turner
-*/
+ * Please see tcpprep.c for license information.
+ *
+ * Copyright (c) 2001 Aaron Turner
+ */
 
 #ifndef __CACHE_H__
 #define __CACHE_H__
@@ -12,20 +12,21 @@
 #define CACHEVERSION "01"
 
 struct cache_type {
-  char data[CACHEDATASIZE];
-  unsigned int bits;
-  struct cache_type *next;
+	char data[CACHEDATASIZE];
+	unsigned int bits;
+	struct cache_type *next;
 };
 
- /* cache_file_header
-    Data structure defining a file as a tcpprep cache file and it's version
-
-    If you need to enhance this struct, do so AFTER the version field and 
-    be sure to increment  CACHEVERSION
+/*
+ * cache_file_header Data structure defining a file as a tcpprep cache file
+ * and it's version
+ * 
+ * If you need to enhance this struct, do so AFTER the version field and be sure
+ * to increment  CACHEVERSION
  */
 struct cache_file_header {
-  char magic[8]; 
-  char version[3]; 
+	char magic[8];
+	char version[3];
 };
 
 typedef struct cache_type CACHE;
