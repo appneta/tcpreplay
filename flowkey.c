f477e23740b49b9561addb8d9e47f7f125369fd2
Aaron Turner <synfinatic@gmail.com>
2003-06-01 23:52:49 +0000

    rewrite rbkeygen() so key is an always unique 12byte char based on the flow's 4-tuple.  Provide pkeygen() which generates a pseudo key suitible for printing which is unique enough for debugging purposes

--- flowkey.c
+++ flowkey.c
@@ -1,4 +1,4 @@
-/* $Id: flowkey.c,v 1.1 2003/05/29 21:58:12 aturner Exp $ */
+/* $Id: flowkey.c,v 1.2 2003/06/01 23:52:49 aturner Exp $ */
 
 /*
  * Copyright (c) 2003 Aaron Turner.
@@ -16,56 +16,89 @@
 
 /*
  * takes in a packet from the IP header on, and generates a unique key
- * for it. using the following formula:
- * u_int64_t = (lowport << 16 ^ highport) << 32 ^ (sip ^ dip)
+ * for the redblack tree. Uses the following formula:
+ * char key[12] = highip + lowip + highport + lowport
+ * returns 1 on success, 0 on fail
  */
-u_int64_t
-rbkeygen(ip_hdr_t *ip, u_char proto, void *l4)
+int
+rbkeygen(ip_hdr_t *ip, u_char proto, void *l4, u_char *key)
 {
     tcp_hdr_t *tcp = NULL;
     udp_hdr_t *udp = NULL;
-    u_int64_t result = 0;
-    u_int64_t temp = 0;
 
-    result = ip->ip_dst.s_addr ^ ip->ip_src.s_addr;
-
+    /* copy over the IP addresses, high then low */
+    if (ip->ip_src.s_addr > ip->ip_dst.s_addr) {
+	memcpy(key, &ip->ip_src.s_addr, 4);
+	memcpy(&key[4], &ip->ip_dst.s_addr, 4);
+    } else {
+	memcpy(key, &ip->ip_dst.s_addr, 4);
+	memcpy(&key[4], &ip->ip_src.s_addr, 4);
+    }
+    
+    /* copy over the port, high then low */
     if (proto == IPPROTO_TCP) {
 	tcp = (tcp_hdr_t *)l4;
 	if (tcp->th_sport > tcp->th_dport) {
-	    temp = (tcp->th_dport << 16) | tcp->th_sport;
+	    memcpy(&key[8], &tcp->th_sport, 2);
+	    memcpy(&key[10], &tcp->th_dport, 2);
 	} else {
-	    temp = (tcp->th_sport << 16) | tcp->th_dport;
+	    memcpy(&key[8], &tcp->th_dport, 2);
+	    memcpy(&key[10], &tcp->th_sport, 2);
 	}
 
-	result = (temp << 32) | result;
-
-	dbg(3, "rbkeygen TCP: %s:%hu - %s:%hu => 0x%llx",
+	dbg(3, "rbkeygen TCP: %s:%hu > %s:%hu => 0x%llx",
 	    libnet_addr2name4(ip->ip_src.s_addr, LIBNET_DONT_RESOLVE),
 	    ntohs(tcp->th_sport),
 	    libnet_addr2name4(ip->ip_dst.s_addr, LIBNET_DONT_RESOLVE),
-	    ntohs(tcp->th_dport), result);
+	    ntohs(tcp->th_dport), pkeygen(key));
 
     } else if (proto == IPPROTO_UDP) {
 	udp = (udp_hdr_t *)l4;
 	if (udp->uh_sport > udp->uh_dport) {
-	    temp = (udp->uh_dport << 16) | udp->uh_sport;
+	    memcpy(&key[8], &udp->uh_sport, 2);
+	    memcpy(&key[10], &udp->uh_dport, 2);
 	} else {
-	    temp = (udp->uh_sport << 16) | udp->uh_dport;
+	    memcpy(&key[8], &udp->uh_dport, 2);
+	    memcpy(&key[10], &udp->uh_sport, 2);
 	}
 
-	result = (temp << 32) | result;
-
-	dbg(3, "rbkeygen UDP: %s:%u - %s:%u => 0x%llx",
+	dbg(3, "rbkeygen UDP: %s:%u > %s:%u => 0x%llx",
 	    libnet_addr2name4(ip->ip_src.s_addr, LIBNET_DONT_RESOLVE),
 	    ntohs(udp->uh_sport),
 	    libnet_addr2name4(ip->ip_dst.s_addr, LIBNET_DONT_RESOLVE),
-	    ntohs(udp->uh_dport), result);
+	    ntohs(udp->uh_dport), pkeygen(key));
 	
     } else {
-	warnx("You tried to get a rbkey for a non-TCP/UDP packet!");
+	warnx("You tried to rbkeygen() for a non-TCP/UDP packet!");
 	return(0);
     }
 
-    return(result);
+    return(1);
 
+}
+
+/*
+ * pseudo-key gen.  Generates a 64bit key suitable for printing via 0x%llx
+ * since we can't print the real 12 byte rbkey
+ */
+
+u_int64_t
+pkeygen(u_char key[])
+{
+    u_int32_t ip1, ip2;
+    u_int16_t port1, port2;
+    u_int64_t result = 0, temp = 0;
+
+    memcpy(&ip1, &key, 4);
+    memcpy(&ip2, &key[4], 4);
+    memcpy(&port1, &key[8], 2);
+    memcpy(&port2, &key[10], 2);
+
+    result = ip1 ^ ip2;
+
+    temp = (port1 << 16) | port2;
+        
+    result = (temp << 32) | result;
+
+    return(result);
 }
--- flowkey.h
+++ flowkey.h
@@ -1,4 +1,4 @@
-/* $Id: flowkey.h,v 1.2 2003/05/30 19:27:57 aturner Exp $ */
+/* $Id: flowkey.h,v 1.3 2003/06/01 23:52:49 aturner Exp $ */
 
 /*
  * Copyright (c) 2001, 2002, 2003 Aaron Turner.
@@ -14,7 +14,7 @@
 #include "flowreplay.h"
 #include "tcpreplay.h"
 
-
-u_int64_t rbkeygen(ip_hdr_t *, u_char, void *);
+int rbkeygen(ip_hdr_t *, u_char, void *, u_char *);
+u_int64_t pkeygen(u_char []);
 
 #endif
