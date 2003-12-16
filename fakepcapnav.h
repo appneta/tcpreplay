09c89e90edce62f5e8479289022b73f721c9ad02
Aaron Turner <synfinatic@gmail.com>
2003-12-16 03:24:49 +0000

    add fakepcapnav code to allow compile w/o libpcapnav

---
+++ fakepcapnav.h
@@ -0,0 +1,57 @@
+/* $Id: fakepcapnav.h,v 1.1 2003/12/16 03:24:49 aturner Exp $ */
+
+/*
+ * Copyright (c) Aaron Turner.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *    This product includes software developed by Anzen Computing, Inc.
+ * 4. Neither the name of Anzen Computing, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+ 
+#ifndef _FAKEPCAPNAV_H_
+#define _FAKEPCAPNAV_H_
+
+#include "config.h"
+#include <pcap.h>
+
+#ifndef HAVE_PCAPNAV
+
+typedef struct pcapnav pcapnav_t;
+	
+struct pcapnav {
+	pcap_t *pcap;
+};
+
+void pcapnav_init(void);
+pcapnav_t *pcapnav_open_offline(const char *);
+void pcapnav_close(pcapnav_t *);
+pcap_t *pcapnav_pcap(pcapnav_t *);
+
+#endif
+
+#endif
