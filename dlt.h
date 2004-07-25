04faf19dcb2668e5ba779c12edf8180825bbfcce
Aaron Turner <synfinatic@gmail.com>
2004-07-25 23:38:54 +0000

    Anzen has nothing to do with our new dlt.h so don't include them in the license

--- dlt.h
+++ dlt.h
@@ -1,10 +1,8 @@
-/* $Id: dlt.h,v 1.1 2004/07/14 05:08:33 aturner Exp $ */
+/* $Id: dlt.h,v 1.2 2004/07/25 23:38:54 aturner Exp $ */
 
 /*
  * Copyright (c) 2004 Aaron Turner
  * All rights reserved.
- *
- * Copyright (c) 1999 Anzen Computing. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -15,10 +13,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  *    notice, this list of conditions and the following disclaimer in the
  *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *       This product includes software developed by Anzen Computing, Inc.
- * 4. Neither the names of the copyright owners nor the names of its
+ * 3. Neither the names of the copyright owners nor the names of its
  *    contributors may be used to endorse or promote products derived from
  *    this software without specific prior written permission.
  *
