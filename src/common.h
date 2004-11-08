187de2bff41a5d81157186b91726536dc6130792
Aaron Turner <synfinatic@gmail.com>
2004-11-08 23:42:00 +0000

    remove timer.h, fix include of config.h

--- src/common.h
+++ src/common.h
@@ -1,6 +1,6 @@
 #ifndef __COMMON_H__
 #define __COMMON_H__
-#include "../config.h"
+#include "config.h"
 #include "common/cache.h"
 #include "common/cidr.h"
 #include "common/err.h"
@@ -9,7 +9,6 @@
 #include "common/fakepoll.h"
 #include "common/list.h"
 #include "common/services.h"
-#include "common/timer.h"
 #include "common/tree.h"
 #include "common/utils.h"
 #include "common/xX.h"
