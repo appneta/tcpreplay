5a9a3d675481bef577ae282910374425f46568c0
Aaron Turner <synfinatic@gmail.com>
2005-04-20 16:36:14 +0000

    pick up libopts.m4 from libopts/

--- autogen.sh
+++ autogen.sh
@@ -3,5 +3,5 @@
 rm -f aclocal.m4 2>/dev/null
 aclocal 
 # aclocal doesn't pick up libopts.m4 so we do it manually
-cat config/libopts.m4 >> aclocal.m4
+cat libopts/libopts.m4 >> aclocal.m4
 automake && autoconf
