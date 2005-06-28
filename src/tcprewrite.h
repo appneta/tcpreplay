8004b7e2755a77bbf0197d6b777b64d963d03f05
Aaron Turner <synfinatic@gmail.com>
2005-06-28 05:25:16 +0000

    update copyright

--- src/tcprewrite.h
+++ src/tcprewrite.h
@@ -1,7 +1,7 @@
 /* $Id:$ */
 
 /*
- * Copyright (c) 2004 Aaron Turner <aturner@pobox.com>.
+ * Copyright (c) 2004-2005 Aaron Turner <aturner@pobox.com>.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
