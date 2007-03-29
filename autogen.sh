274f9174743a9dd8999880b0f6743b51045ae4e1
Aaron Turner <synfinatic@gmail.com>
2007-03-29 05:12:05 +0000

    call autoconf, automake, autoconf to bootstrap which seems like the "best" way. sigh.  refs #47

--- autogen.sh
+++ autogen.sh
@@ -4,4 +4,7 @@ rm -f aclocal.m4 2>/dev/null
 aclocal 
 # aclocal doesn't pick up libopts.m4 so we do it manually
 cat libopts/m4/libopts.m4 libopts/m4/liboptschk.m4  >> aclocal.m4
-automake && autoconf
+autoconf
+automake
+autoconf
+
