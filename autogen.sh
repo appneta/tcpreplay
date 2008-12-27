61f841e5a0a28d650e95d078a198382e6546403b
Aaron Turner <synfinatic@gmail.com>
2008-12-27 21:29:26 +0000

    merge autofoo patch from Joerg. refs #340

--- autogen.sh
+++ autogen.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 rm -f aclocal.m4 2>/dev/null
-aclocal 
+aclocal  -I libopts/m4/
 # aclocal doesn't pick up libopts.m4 so we do it manually
-cat libopts/m4/libopts.m4 libopts/m4/liboptschk.m4  >> aclocal.m4
-autoconf
+#cat libopts/m4/libopts.m4 libopts/m4/liboptschk.m4  >> aclocal.m4
+libtoolize || glibtoolize
+autoheader
 automake
 autoconf
 
