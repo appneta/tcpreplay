ed94d54929e456afb5f5be2e98065a276e5949d2
Aaron Turner <synfinatic@gmail.com>
2009-01-01 23:04:47 +0000

    delete generated files before running.  fixes #340

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-
+rm -f config/config.guess config/config.sub config/ltmain.sh 2>/dev/null
 rm -f aclocal.m4 2>/dev/null
 aclocal  -I libopts/m4/
 # aclocal doesn't pick up libopts.m4 so we do it manually
