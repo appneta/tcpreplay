8cfc5c8f5e8293a2fb12bd59a628408bac1ff555
Aaron Turner <synfinatic@gmail.com>
2009-01-04 20:17:39 +0000

    more gracefully deal w/ os x glibtoolize.  fixes #340

--- autogen.sh
+++ autogen.sh
@@ -2,9 +2,12 @@
 rm -f config/config.guess config/config.sub config/ltmain.sh 2>/dev/null
 rm -f aclocal.m4 2>/dev/null
 aclocal  -I libopts/m4/
-# aclocal doesn't pick up libopts.m4 so we do it manually
-#cat libopts/m4/libopts.m4 libopts/m4/liboptschk.m4  >> aclocal.m4
-libtoolize || glibtoolize
+if test -x "`which libtoolize`" ; then
+    libtoolize
+else
+    # Necessary under OS X
+    glibtoolize
+fi
 autoheader
 automake
 autoconf
