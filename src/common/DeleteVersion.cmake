fd76093b8b5d71610d9eaa8bfa9e1b2993043d46
Aaron Turner <synfinatic@gmail.com>
2009-02-26 22:25:15 +0000

    mostly fix svn_version.c. remove old autotools cruft. refs #351

---
+++ src/common/DeleteVersion.cmake
@@ -0,0 +1,1 @@
+file(REMOVE svn_version.c)
---
+++ src/common/GenerateVersion.cmake
@@ -0,0 +1,15 @@
+find_program(SVNVERSION svnversion)
+set(WC_REVISION unknown)
+if(EXISTS ${CMAKE_SOURCE_DIR}/.svn)
+    if(SVNVERSION)
+        execute_process(COMMAND ${SVNVERSION} -n .
+            WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+            OUTPUT_VARIABLE WC_REVISION)
+    else(SVNVERSION)
+        message(STATUS "Missing SVNVERSION! ${SVNVERSION}")
+    endif(SVNVERSION)
+else(EXISTS ${CMAKE_SOURCE_DIR}/.svn)
+    set(WC_REVISION exported)
+endif(EXISTS ${CMAKE_SOURCE_DIR}/.svn)
+
+configure_file(${CMAKE_SOURCE_DIR}/svn_version.tmpl ${CMAKE_SOURCE_DIR}/svn_version.c @ONLY)
