19d8a467210b15acd4584677f610f7e68da5c651
Aaron Turner <synfinatic@gmail.com>
2013-11-27 23:01:30 -0800

    use glibtool on OSX because OSX libtool sucks! fixes #116

--- autogen.sh
+++ autogen.sh
@@ -2,11 +2,11 @@
 rm -f config/config.guess config/config.sub config/ltmain.sh 2>/dev/null
 rm -f aclocal.m4 2>/dev/null
 aclocal  -I libopts/m4/
-if test -x "`which libtoolize`" ; then
-    libtoolize --copy
-else
+if test -x "`which glibtoolize`" ; then
     # Necessary under OS X
-    glibtoolize --copy
+    glibtoolize --copy --automake --force
+else
+    libtoolize --copy
 fi
 autoheader
 automake --add-missing --copy
