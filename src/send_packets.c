d0bac738785c0a7c74c5ff12d9367ff80d313859
Pekka Pietikainen <pekka@pietikainen.org>
2014-10-20 16:05:43 +0300

    ... also for dualfiles

--- src/send_packets.c
+++ src/send_packets.c
@@ -680,9 +680,6 @@ send_dual_packets(tcpreplay_t *ctx, pcap_t *pcap1, int cache_file_idx1, pcap_t *
     bool do_not_timestamp = options->speed.mode == speed_topspeed ||
             (options->speed.mode == speed_mbpsrate && !options->speed.speed);
 
-    init_timestamp(&ctx->stats.last_time);
-    init_timestamp(&ctx->stats.last_print);
-    init_timestamp(&ctx->stats.end_time);
     start_us = TIMEVAL_TO_MICROSEC(&ctx->stats.start_time);
 
     if (options->preload_pcap) {
