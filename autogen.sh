0c75080fcce8977cfc126488397b632e0d012f50
Fred Klassen <fklassen@appneta.com>
2017-03-11 11:51:09 -0800

    #299 updates for code climate and codacy (#301)
    * #299 update README.md for code climate
    
    * Update README.md
    
    * Add Codacy badge
    
    * Update .codeclimate.yml
    
    disable fixme

--- autogen.sh
+++ autogen.sh
@@ -2,7 +2,7 @@
 rm -f config/config.guess config/config.sub config/ltmain.sh 2>/dev/null
 rm -f aclocal.m4 2>/dev/null
 aclocal  -I libopts/m4/
-if test -x "`which glibtoolize`" ; then
+if test -x "$(which glibtoolize)" ; then
     # Necessary under OS X
     glibtoolize --copy --automake --force
 else
