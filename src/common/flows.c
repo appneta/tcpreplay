f63904053e9d69edf37a971070ba1746c746eaa1
dumprop <github@rop.cx>
2021-05-09 21:06:46 +0300

    removed test code

--- src/common/flows.c
+++ src/common/flows.c
@@ -23,7 +23,6 @@
 #include <string.h>
 #include "../../lib/sll.h"
 
-#define JUNIPER_FLAG_NO_L2          0x02     /* L2 header */
 #define JUNIPER_FLAG_EXT            0x80     /* Juniper extensions present */
 #define JUNIPER_PCAP_MAGIC          "MGC"
 
@@ -239,18 +238,6 @@ flow_entry_type_t flow_decode(flow_hash_table_t *fht, const struct pcap_pkthdr *
         } else {
             l2_len = 4; /* MGC + flags (no header extensions) */
         }
-
-        if ((pktdata[3] & JUNIPER_FLAG_NO_L2) == JUNIPER_FLAG_NO_L2) {
-            /* no L2 header present - eth_hdr_offset is actually IP offset */
-            uint32_t ip_hdr_offset = l2_len;
-            if (pkthdr->caplen < ip_hdr_offset + 1)
-                return 0;
-            if ((pktdata[ip_hdr_offset] >> 4) == 4)
-                return ETHERTYPE_IP;
-            else if ((pktdata[ip_hdr_offset] >> 4) == 6)
-                return ETHERTYPE_IP6;
-            else
-                return 0;
         }
         /* fall through */
     case DLT_EN10MB:
